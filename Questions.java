554b6d8346d8d9dd3ebefe06ceff4503c2d7d887
cnovitsky5 <44688470+cnovitsky5@users.noreply.github.com>
2018-11-20 15:22:18 -0500

    Add files via upload

---
+++ Questions.java
@@ -0,0 +1,36 @@
+
+public class Questions {
+	
+	public static String[][] init_Questions() {
+		String[][] life = new String[8][4];
+		life[0][0] = "This is question 0,0";
+		life[0][1] = "This is question 0,1";
+		life[0][2] = "This is question 0,2";
+		life[0][3] = "This is question 0,3";
+		life[1][1] = "This is question 1,1";
+		life[1][2] = "This is question 1,2";
+		life[1][3] = "This is question 1,3";
+		life[2][2] = "This is question 2,2";
+		life[2][3] = "This is question 2,3";
+		life[3][2] = "This is question 3,2";
+		life[3][3] = "This is question 3,3";
+		life[4][3] = "This is question 4,3";
+		life[5][3] = "This is question 5,3";
+		life[6][3] = "This is question 6,3";
+		life[7][3] = "This is question 7,3";
+		return life;
+	}
+	public static String[]printOptions(int row, int column){
+		String[] option = new String[2];
+		//Do this with if(row==x && column==y) so that we can specify the answers for each question
+		option[0] = "This is Option 1 for question "+row+","+column;
+		option[1] = "This is Option 2 for question "+row+","+column;
+		return option;
+		
+	}
+	protected int nextQuestion;
+	
+	public static String printQuestion(String[][] life, int row, int column) {
+		return life[row][column];
+	}
+}
